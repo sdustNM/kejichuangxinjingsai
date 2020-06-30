8488d30d3c10c653b35d841b4479ee0a3c79e666
redrainer <49148979@qq.com>
2020-06-30 10:43:51 +0800

    wc modif

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import zhCN from 'antd/es/locale/zh_CN'
 import 'antd/dist/antd.css'
 
 // redux 使用举例
-import store from  './redux/store'
+//import store from  './redux/store'
 // import {counterAddAction} from './redux/action/counter_action'
 // import { loadPostsAction} from './redux/action/post_action';
-import { loadDepartmentListAction} from './redux/action/departmentList_action';
+//import { loadDepartmentListAction} from './redux/action/departmentList_action';
 
 // console.log(store);
 // console.log(store.getState())
 
 // store.dispatch(counterAddAction);
 // store.dispatch(loadPostsAction)
-store.dispatch(loadDepartmentListAction);
+// store.dispatch(loadDepartmentListAction);
 
 setTimeout(() => {
     console.log('last');
@@ -26,13 +26,13 @@ console.log(store.getState())
 }, 1000);
 
 
-// ReactDOM.render(
-//   <ConfigProvider locale={zhCN}>
-//     <App />
-//   </ConfigProvider>
-//   ,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>
+  ,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
