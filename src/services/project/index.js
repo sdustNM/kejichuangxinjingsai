31e8566f75c1b6143ab69dcb92e3e05272d0492b
redrainer <49148979@qq.com>
2020-07-23 17:27:09 +0800

    20200723

--- src/services/project/index.js
+++ src/services/project/index.js
@@ -4,6 +4,12 @@ import { get, post, del } from '../../utils/request'
 export function getSimpleProjectList(params){
   return get('/administer/getSimpleProjectList', params)
 }
+
+//根据 比赛ID、学号、专家ID 获取参赛项目列表，支持分页,不带附件信息
+export function getSimpleProjectListForExpert(params){
+  return get('/administer/getSimpleProjectListForExpert', params)
+}
+
 //根据项目ID获取参赛项目内容
 export function getProjectInfoByID(params){
   return get('/administer/getProjectInfoByID', params)
