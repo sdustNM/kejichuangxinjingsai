238a38c4a3455c51c6a3e0f763d89b3761795be2
redrainer <49148979@qq.com>
2020-11-18 14:54:38 +0800

    wc add SelectStudentComplete

--- src/services/Archievements/index.js
+++ src/services/Archievements/index.js
@@ -3,3 +3,8 @@ import { get } from '../../utils/request'
 export function getAllManByFuzzy(params){
   return get('/administer/getAllManByFuzzy', params)
 }
+
+
+export function getStudentsByFuzzy(params){
+    return get('/administer/getStudentsByFuzzy', params)
+  }
