32c46fcdd9a3b04c03fc5a9ce15c022512dd72d0
redrainer <49148979@qq.com>
2020-12-14 21:35:40 +0800

    ss

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,9 +4,9 @@ import config from '../../public/config.json'
 import Axios from 'axios'
 
 ///export const appRoot = config.approot
-export const appRoot = 'https://kcpt.sdust.edu.cn/manager'
+//export const appRoot = 'https://kcpt.sdust.edu.cn/manager'
 //export const appRoot = 'http://192.168.34.201:4000'
-//export const appRoot = 'http://localhost:5000'
+export const appRoot = 'http://localhost:5000'
 export const baseURL = `${appRoot}/api`
 
 
